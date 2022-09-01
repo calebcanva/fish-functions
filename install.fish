1923901277ef17026e5fafa9674c64ee4dd926c8
Caleb Charles <caleb@canva.com>
2022-09-02 02:08:14 +1000

    Update install to clean broken

--- install.fish
+++ install.fish
@@ -1,7 +1,18 @@
 cd (dirname (status --current-filename))
-for dir in "functions" "completions"
-    set -l files (ls $dir | grep '.fish\|.mp3')
-    for file in $files
-        ln -s (pwd)/$dir/$file ~/.config/fish/$dir/$file
+for DIR in functions completions
+    echo (set_color normal)"Installing $DIR:"
+    set -l FILES (ls $DIR | grep '.fish\|.mp3')
+    echo (set_color green)(string join (set_color normal)", "(set_color green) $FILES)(set_color normal)"."
+    for FILE in $FILES
+        ln -s (pwd)/$DIR/$FILE ~/.config/fish/$DIR/$FILE 2>/dev/null
+    end
+    set -l ALL_FILES (ls ~/.config/fish/$DIR/)
+    for FILE in $ALL_FILES
+        set -l LINKED_FILE (readlink -f ~/.config/fish/$DIR/$FILE)
+        # Remove symlinks that don't work any more
+        if not test -f $LINKED_FILE
+            rm -f ~/.config/fish/$DIR/$FILE
+        end
     end
 end
+echo (set_color -i grey)"Done..."
