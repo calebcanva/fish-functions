7bc777117baa9ed848fcf40d006eae7428c1b386
Caleb Charles <caleb@canva.com>
2022-12-20 10:02:11 +1100

    token update

--- functions/save-token.fish
+++ functions/save-token.fish
@@ -1,3 +1,5 @@
 function save-token --description 'Save token' --argument NAME --argument TOKEN
   echo $TOKEN > ~/.tokens/$NAME
+  sudo chown root ~/.tokens/$NAME
+  sudo chmod 600  ~/.tokens/$NAME
 end
--- functions/token.fish
+++ functions/token.fish
@@ -1,3 +1,3 @@
 function token --description 'Get token' --argument TOKEN
-    cat (string join "" ~/.tokens/ $TOKEN)
+    sudo cat (string join "" ~/.tokens/ $TOKEN)
 end
