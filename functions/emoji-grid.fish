eb652af93925bd74ff58b5b3ee1049f32fbd21d7
Caleb Charles <caleb@canva.com>
2022-12-23 10:35:45 +1100

    Add large emoji func

---
+++ functions/emoji-grid.fish
@@ -0,0 +1,21 @@
+function emoji-grid --argument NAME --argument SIZE --description 'Makes a large emoji for use in slack'
+    if test -d $SIZE 
+        set $SIZE 3x3
+    end
+    set -l DIMS (string split "x" $SIZE)
+    echo $DIMS
+    set -l OUT (mktemp)
+    set -l i 1
+    for y in (seq 1 $DIMS[2])
+        set -l LINE ""
+        for x in (seq 1 $DIMS[1])
+            set LINE "$LINE:$NAME$i:"
+            set i (math $i + 1)
+        end
+        echo $LINE
+        echo $LINE >> $OUT
+    end
+    cat $OUT | pbcopy
+    echo (set_color -i grey)Copied to clipboard!
+    rm -rf $OUT`
+end
--- functions/play-sound.fish
+++ functions/play-sound.fish
@@ -1,5 +1,13 @@
 function play-sound --argument NAME
-    set -l SOUND (dirname (status --current-filename))"/$NAME.mp3"
+    set -l SOUND (echo -n (find -E ~/.config/fish -regex '.*\.(mp3|wav)' | grep $NAME))
+    if not test (count $SOUND) -eq 1
+        if test (count $SOUND) -lt 1
+            echo "No sound file found for $NAME"
+        else
+            echo "Too many files found: $SOUND"
+        end
+        return 1
+    end
     if test -f $SOUND
         afplay $SOUND >/dev/null 2>&1 &
     end
