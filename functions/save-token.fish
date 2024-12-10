1c3ce9ba0948820143c9554778e0a2ec13fddc2e
Caleb Charles <caleb@canva.com>
2024-12-10 16:39:48 +1100

    .

--- functions/save-token.fish
+++ functions/save-token.fish
@@ -1,5 +1,9 @@
 function save-token --description 'Save token' --argument NAME --argument TOKEN
-  echo $TOKEN > ~/.tokens/$NAME
+  if test -f filename
+      sudo chown (whoami) ~/.tokens/$NAME
+      sudo chmod u+w ~/.tokens/$NAME
+  end
+  echo $TOKEN >~/.tokens/$NAME
   sudo chown root ~/.tokens/$NAME
-  sudo chmod 600  ~/.tokens/$NAME
+  sudo chmod 600 ~/.tokens/$NAME
 end
