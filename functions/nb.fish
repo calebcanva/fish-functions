32e00cf9e6eb6110a8695374fece640351584cac
Caleb Charles <caleb@canva.com>
2025-01-03 11:11:34 +1100

    optimise nb

--- functions/nb.fish
+++ functions/nb.fish
@@ -1,16 +1,21 @@
 function nb --description 'Create a fresh branch off green'
     begin
-        git fetch origin green
-        read -l -P 'Branch name: ' BRANCH_NAME
-        if test $status -gt 0
+        set -l BRANCH_NAME
+        if set -q argv[1]
+            set BRANCH_NAME (string join " " $argv)
+        else
+            # For some reason I have to put it in another variable otherwise `read` won't override :sadge:
+            read -l -P 'Branch name: ' READ
+            set BRANCH_NAME $READ
+        end
+        if test $status -gt 0; or not test $BRANCH_NAME
+            echo Some details missing. Exiting...
             return $status
         end
-        if test $BRANCH_NAME
-            set FULL_BRANCH_NAME (slugify (whoami)"-$BRANCH_NAME")
-            echo Checking out new branch $FULL_BRANCH_NAME
-            git checkout -b $FULL_BRANCH_NAME origin/green --no-track
-        else
-            echo Some details missing. Exiting...
-        end
+        echo (set_color -i grey)Fetching green...(set_color normal)
+        git fetch origin green
+        set FULL_BRANCH_NAME (slugify (whoami)"-$BRANCH_NAME")
+        echo Checking out new branch $FULL_BRANCH_NAME
+        git checkout -b $FULL_BRANCH_NAME origin/green --no-track
     end
 end
