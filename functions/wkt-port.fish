3adcba96faf41ef176bc452e93b97f911f2dc145
Caleb Charles <caleb@canva.com>
2025-02-19 13:23:27 +1100

    Update wkt-port.fish

--- functions/wkt-port.fish
+++ functions/wkt-port.fish
@@ -1,25 +1,26 @@
 function wkt-port
+    set -l PORT 9090
     set -l WORKTREE (git rev-parse --show-toplevel)
     set -l NUM (string match -r "[0-9]" (string split "-" $WORKTREE)[-1] != "")
     switch $NUM
     case 2
-        set -g PORT 9091
+        set -l PORT 9091
     case 3
-        set -g PORT 9092
+        set -l PORT 9092
     case 4
-        set -g PORT 9093
+        set -l PORT 9093
     case 5
-        set -g PORT 9094
+        set -l PORT 9094
     case 6
-        set -g PORT 9095
+        set -l PORT 9095
     case 7
-        set -g PORT 9096
+        set -l PORT 9096
     case 8
-        set -g PORT 9097
+        set -l PORT 9097
     case 9
-        set -g PORT 9098
+        set -l PORT 9098
     case '*'
-        set -g PORT 9090
+        set -l PORT 9090
     end
     return $PORT
 end
