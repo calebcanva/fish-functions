895c2b082e40c1022c3a64f2ff993f91db97c7e6
Caleb Charles <caleb@canva.com>
2025-12-17 11:44:58 +1100

    Update freshen.fish

--- functions/freshen.fish
+++ functions/freshen.fish
@@ -1,9 +1,9 @@
 function freshen --description 'Freshen the current branch from the latest green'
     if pr-train --exists --silent
         echo (set_color -i grey)Pr train detected...(set_color normal)
-        git fetch origin green && pr-train merge
+        git fetch origin master && pr-train merge
     else
         echo (set_color -i grey)Going to merge the latest green...(set_color normal)
-        git fetch origin green && git merge --no-edit origin/green && git push
+        git fetch origin master && git merge --no-edit origin/green && git push
     end
 end
