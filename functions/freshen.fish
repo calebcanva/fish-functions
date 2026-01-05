f78a32167e06259b2f758bef71d4ea1c85a33059
Caleb Charles <caleb@canva.com>
2026-01-05 12:26:44 +1100

    Update freshen.fish

--- functions/freshen.fish
+++ functions/freshen.fish
@@ -3,7 +3,7 @@ function freshen --description 'Freshen the current branch from the latest maste
         echo (set_color -i grey)Pr train detected...(set_color normal)
         git fetch origin master && pr-train merge
     else
-        echo (set_color -i grey)Going to merge the latest green...(set_color normal)
+        echo (set_color -i grey)Going to merge the latest master...(set_color normal)
         git fetch origin master && git merge --no-edit origin/master && git push
     end
 end
